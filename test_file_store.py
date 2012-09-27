75d367b6614fdd4e8e3e8eff5a4aa6dadfc2ea3c
Charles Haynes <haynes+git@biz.fifth-mountain.com>
2012-09-27 21:21:23 +0200

    fixed file store test names

--- test_file_store.py
+++ test_file_store.py
@@ -11,30 +11,30 @@ __author__ = 'chaynes'
 import unittest
 
 @patch('io.open', MagicMock())
-class FileDictionaryTests(unittest.TestCase):
+class FileStoreTests(unittest.TestCase):
     def setUp(self):
         self.hasher = Mock(return_value=TEST_DIGEST)
         self.on_duplicate = Mock()
-        self.file_dictionary = file_store.FileStore(self.on_duplicate, self.hasher)
+        self.file_store = file_store.FileStore(self.on_duplicate, self.hasher)
 
     def test_can_add_file(self):
-        self.file_dictionary.Add(TEST_FILE_NAME)
+        self.file_store.Add(TEST_FILE_NAME)
 
         self.assertEqual(1, self.hasher.call_count)
 
     def test_adding_duplicate_key_calls_on_duplicate(self):
-        self.file_dictionary.Add(TEST_FILE_NAME)
-        self.file_dictionary.Add(TEST_FILE_NAME2)
+        self.file_store.Add(TEST_FILE_NAME)
+        self.file_store.Add(TEST_FILE_NAME2)
 
         self.on_duplicate.assert_called_once_with(TEST_FILE_NAME2)
 
     def test_adding_duplicate_digest_creates_duplicate_value(self):
-        self.file_dictionary.Add(TEST_FILE_NAME)
+        self.file_store.Add(TEST_FILE_NAME)
         self.hasher.return_value = TEST_DIGEST2
-        self.file_dictionary.Add(TEST_FILE_NAME2)
+        self.file_store.Add(TEST_FILE_NAME2)
 
         test_hashes = {TEST_DIGEST, TEST_DIGEST2}
-        self.assertSetEqual(test_hashes, self.file_dictionary.Hashes())
+        self.assertSetEqual(test_hashes, self.file_store.Hashes())
 
 if __name__ == '__main__':
     unittest.main()
